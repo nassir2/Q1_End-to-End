100ffacb21b45882a2e1d11169c5c9a2c147744e
Abdi Nassir <50337444+nassir2@users.noreply.github.com>
2021-04-13 12:45:00 -0500

    Delete src directory

--- src/main/java/com/gm/dao/StudentCourseDAO.java
+++
@@ -1,8 +0,0 @@
-package com.gm.dao;
-
-import com.gm.model.StudentCourse;
-
-public interface StudentCourseDAO
-{
-	public StudentCourse getstudentCourseDetails(int stuId);
-}
--- src/main/java/com/gm/service/StudentCourseService.java
+++
@@ -1,8 +0,0 @@
-package com.gm.service;
-
-import com.gm.model.StudentCourse;;
-
-public interface StudentCourseService 
-{
-	public StudentCourse getstudentCourseinfo(int stuId);
-}
